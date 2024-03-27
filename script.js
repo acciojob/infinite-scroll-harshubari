ff41001bab44aedbc021744e9d7d9a96d8a904a7
harshubari <121845828+harshubari@users.noreply.github.com>
2024-03-27 10:20:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
+let ol = document.getElementById("infi-list");
 
+let scrollHeit = 1;
+const loop = () => {
+  for (let i = 1; i <= 10; i++) {
+    let created = document.createElement("li");
+    created.innerText = `${"List Item " + scrollHeit ++}`;
+    ol.appendChild(created);
+  }
+};
+ol.addEventListener("scroll", (e) => {
+  let totalScrollHeight = ol.scrollHeight;
+  let visible = ol.clientHeight;
+  let scrollHeight = ol.scrollTop;
+  let rem = totalScrollHeight - visible - scrollHeight;
+  
+  if (rem <= 0) {
+    loop();
+  }
+});
+loop();
